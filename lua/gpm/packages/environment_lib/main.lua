b5e8ee3b8ecd353f4348dbbf432684f41a36db7c
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-11 04:29:53 +0400

    2.0.0

--- lua/gpm/packages/environment_lib/main.lua
+++ lua/gpm/packages/environment_lib/main.lua
@@ -1,58 +1,120 @@
 local assert = assert
 local type = type
 
-environment = environment or {
-    ["Environments"] = {},
-    ["Functions"] = {
-        ["null"] = function()end
-    }
+module( "environment", package.seeall )
+
+Environments = {}
+Functions = {
+    ["null"] = function() end
 }
 
-function environment.saveFunc( name, func, override )
+--[[-------------------------------------------------------------------------
+    Functions Caching
+---------------------------------------------------------------------------]]
+
+function saveFunc( name, func, override )
 	assert( type( name ) == "string", "bad argument #1 (string expected)" )
     assert( name ~= "null", "You can't just go ahead and overwrite a 'null' function." )
     assert( type( func ) == "function", "bad argument #2 (function expected)" )
 
-    if (environment["Functions"][name] == nil) or (override == true) then
-        environment["Functions"][name] = func
+    if (Functions[ name ] == nil) or (override == true) then
+        Functions[ name ] = func
     end
 
-    return environment["Functions"][name]
+    return Functions[ name ]
 end
 
-function environment.loadFunc( name )
-    return environment["Functions"][name] or environment["Functions"]["null"]
+function loadFunc( name )
+    return Functions[ name ] or Functions["null"]
 end
 
-function environment.new( name, builder, override )
+function removeFunc( name )
 	assert( type( name ) == "string", "bad argument #1 (string expected)" )
-
-    local env = {}
-    if type( builder ) == "function" then
-        env = builder( name )
-    end
-
-    env["__environmentName"] = name
-
-    if ((environment["Environments"][name] == nil) and type( env ) == "table") or (override == true) then
-        environment["Environments"][name] = env
-    end
-
-    return environment["Environments"][name] or {}
+    assert( name ~= "null", "You can't just go ahead and remove a 'null' function." )
+    Functions[ name ] = nil
 end
 
-function environment.load( name )
-    return environment["Environments"][name] or {}
-end
+--[[-------------------------------------------------------------------------
+    Environments
+---------------------------------------------------------------------------]]
 
-function environment.getName( env )
-    return type( env ) == "table" and env["__environmentName"] or nil
-end
-
-function isEnvironment( any )
-    return type( any ) == "table" and (any["__environmentName"] != nil)
-end
-
-function environment.global()
+function global()
     return _G
+end
+
+do
+
+    local ENV = {}
+    ENV["__index"] = ENV
+    debug.getregistry().Environment = ENV
+
+    do
+
+        function ENV:getID()
+            return self["__env"]["id"]
+        end
+
+        function ENV:getName()
+            return self["__env"]["identifier"]
+        end
+
+        function ENV:__tostring()
+            return "GLua Environment - " .. self:getName() .. " [" .. self:getID() .. "]"
+        end
+
+    end
+
+    do
+        local getmetatable = getmetatable
+        function isEnvironment( any )
+            return getmetatable( any ) == ENV
+        end
+    end
+
+    do
+
+        local emptyTable = {}
+
+        do
+            local table_Count = table.Count
+            local setmetatable = setmetatable
+            function new( any, builder, override )
+                assert( type( any ) == "string", "bad argument #1 (string expected)" )
+
+                if (Environments[ any ] == nil) or (override == true) then
+                    local env = {}
+                    if type( builder ) == "function" then
+                        env = builder( any ) or env
+                    end
+
+                    if ( type( env ) == "table" ) then
+                        env["__env"] = {
+                            ["identifier"] = any,
+                            ["id"] = table_Count( Environments ) + 1
+                        }
+
+                        Environments[ any ] = setmetatable( env, ENV )
+                    end
+                end
+
+                return Environments[ any ] or emptyTable
+            end
+        end
+
+        function load( any )
+            return Environments[ any ] or emptyTable
+        end
+
+    end
+
+end
+
+function getName( env )
+    if isEnvironment( env ) then
+        return env:getName()
+    end
+end
+
+function remove( any )
+    Environments[ any ] = nil
 end
--- lua/gpm/packages/environment_lib/package.lua
+++ lua/gpm/packages/environment_lib/package.lua
@@ -1,7 +1,18 @@
 return {
     name = "Environment Library",
     description = "A library that expands the possibilities of working with environments in GLua.",
-    version = "1.0.0",
-    author = "PrikolMen:-b",
+    version = "2.0.0",
+    contributors = {
+        {
+            name = "PrikolMen:-b",
+            discord = "PrikolMen#3372",
+            email = "prikolmen@pika-soft.ru"
+        },
+        {
+            name = "_ᐱℕᏩĒŁØҜҜ_",
+            discord = "_ᐱℕᏩĒŁØҜҜ_#8486",
+            email = "angonex@pika-soft.ru",
+        }
+    },
     license = "MIT"
 }
